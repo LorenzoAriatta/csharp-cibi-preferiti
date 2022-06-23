ca57e00fbfa4343ca5e91eb8c98900446b5e21c2
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-06-23 14:50:38 +0200

    create array console array.Length

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string[] food = { "pizza", "tiramisù", "focaccia", "pasta", "pollo fritto" };
+
+Console.WriteLine(food.Length);
+
+for(int i = 0; i < food.Length; i++)
+{
+    Console.WriteLine((i+1) + " " + food[i]);
+}
